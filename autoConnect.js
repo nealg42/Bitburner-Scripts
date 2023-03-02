839f51714ab8f6e0e566eb4e4f778976a69c1bab
neal.g <nealg42@gmail.com>
2023-03-01 22:47:33 -0500

    Optimize autoConnect

--- autoConnect.js
+++ autoConnect.js
@@ -1,27 +1,12 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	let servers = ['home'];
-	let routes = new Object;
+	function svrRoute(svr = new String) {
+		let route = [ns.scan(svr)[0]];
+		while (route[0] != 'home') { route.unshift(ns.scan(route[0])[0]); }
+		return route;
+	}
 
-	let delta = 0;
-	do {
-		delta = 0;
-		for (const server of servers) {
-			let serversNew = ns.scan(server);
-			if (server != 'home') { serversNew.shift(); }
-			for (const serverNew of serversNew) {
-				if (!servers.includes(serverNew)) {
-					servers.push(serverNew);
-					let route = [ns.scan(serverNew)[0]];
-					while (route[0] != 'home') { route.unshift(ns.scan(route[0])[0]); }
-					routes[serverNew] = route;
-					delta++;
-				}
-			}
-		}
-	} while (delta > 0);
-
-	for (let node of routes[ns.args[0]]) {
+	for (let node of svrRoute(ns.args[0])) {
 		ns.singularity.connect(node);
 	}
 	ns.singularity.connect(ns.args[0]);
